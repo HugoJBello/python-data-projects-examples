b366a229b45f34ea54399cfb82affe4130e528e5
hugo bello <hjbello.wk@gmail.com>
2018-10-13 22:50:36 +0200

    fix

--- extract_points_from_feature/extract_points_from_features.py
+++ extract_points_from_feature/extract_points_from_features.py
@@ -1,15 +1,26 @@
 from osgeo import ogr
-from osgeo import osr
-src = osr.SpatialReference()
-src.SetWellKnownGeogCS( "WGS84" );
-shapefile = "data/SECC_CPV_E_20111101_01_R_INE_MADRID_cs_epsg_2.shp"
+from pypolyline.util import encode_coordinates
+import polyline
+
+import json
+import urllib.parse
+shapefile = "data/SECC_CPV_E_20111101_01_R_INE_MADRID_cs_epsg.shp"
 data_source = ogr.Open(shapefile,False)  # True allows to edit the shapefile
 layer = data_source.GetLayer()
 
 feature = layer.GetNextFeature()
 while feature:
     geom = feature.GetGeometryRef()
-    print(geom.Centroid().ExportToWkt())
-    print(geom.Boundary().ExportToJson())
-    print(geom.Boundary().ExportToWkt())
+    cusec =feature.GetField("CUSEC")
+    polyline_dic = geom.Boundary().ExportToJson()
+    filename =  "geoJson_output/" + cusec + "__2011_ine_geojson.json"
+    text_file = open(filename, "w")
+    text_file.write(polyline_dic)
+    text_file.close()
+    print(polyline_dic)
+    #encoded_polyline = polyline.encode(polyline_dic["coordinates"],5)
+    #print(encoded_polyline)
+    #url_encoded_polyline = "((" + encoded_polyline + "))"
+    #print(url_encoded_polyline)
+    #print(urllib.parse.quote_plus(url_encoded_polyline))
     feature = layer.GetNextFeature()
